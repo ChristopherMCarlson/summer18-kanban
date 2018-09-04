79cfe9db240f626aaad4968a5cf305b685272938
Christopher Carlson <c.m.carlson13@gmail.com>
2018-09-04 14:53:17 -0600

    editing server routes

--- server/server-assets/routes/list.js
+++ server/server-assets/routes/list.js
@@ -1,5 +1,16 @@
 let router = require('express').Router()
 let Lists = require('../models/list')
 
+router.get('/:boardId', (req, res, next) => {
+  Lists.find({ boardId: req.params.boardId })
+    .then(data => {
+      res.send(data)
+    })
+    .catch(err => {
+      console.log(err)
+      next()
+    })
+})
+
 
 module.exports = router
