9d447a31cb2b37354543d21a23197d719ff766ea
Christopher Carlson <c.m.carlson13@gmail.com>
2018-09-04 14:40:07 -0600

    models complete

--- server/server-assets/models/task.js
+++ server/server-assets/models/task.js
@@ -9,7 +9,6 @@ let commentSchema = new Schema({
 
 let schema = new Schema({
   title: { type: String, required: true },
-  description: { type: String, required: true },
   created: { type: Number, required: true, default: Date.now() },
   authorId: { type: ObjectId, ref: 'User', required: true },
   listId: { type: ObjectId, ref: 'List', required: true },
