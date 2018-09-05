23d42ec93e8236fc30518a44ec3fac3b68ce5eac
Michael Green <michael.green94@hotmail.com>
2018-09-05 14:02:44 -0600

    created create list form

--- client/src/views/Board.vue
+++ client/src/views/Board.vue
@@ -1,5 +1,14 @@
 <template>
   <div class="board">
+    <div>
+      <form class="list-form" @submit.prevent="createList">
+        <label for="title">Title</label>
+        <input name="title" type="text" v-model="formConfig.title">
+        <label for="description">Description</label>
+        <input name="description" type="text" v-model="formConfig.description">
+        <input type="submit">
+      </form>
+    </div>
     <list />
   </div>
 </template>
@@ -9,6 +18,14 @@
 
   export default {
     name: "board",
+    data() {
+      return {
+        formConfig: {
+          title: '',
+          description: ''
+        }
+      }
+    },
     created() {
       //blocks users not logged in
       if (!this.$store.state.user._id) {
