380dacff0d254b36a4d1d0c8dfe41e439f40b8dd
Michael Green <michael.green94@hotmail.com>
2018-09-07 16:10:11 -0600

    styling login page

--- client/src/components/LoginModal.vue
+++ client/src/components/LoginModal.vue
@@ -1,17 +1,17 @@
 <template>
-  <div class="quick-modal">
+  <div class="login-modal">
     <!-- Button trigger modal -->
-    <button :id="buttonId || '' " :style="{ display: isHidden ? 'none' : ''}" type="button" class="btn btn-primary" data-toggle="modal"
-      :data-target="'#'+modalId">
-      +
+    <button type="button" class="modal-button" data-toggle="modal" data-target="#exampleModalCenter">
+      LOGIN/REGISTER
     </button>
 
     <!-- Modal -->
-    <div class="modal fade" :id="modalId" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+    <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle"
+      aria-hidden="true">
       <div class="modal-dialog modal-dialog-centered" role="document">
         <div class="modal-content">
           <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLongTitle">Add New</h5>
+            <h5 class="modal-title" id="exampleModalLongTitle">Login/Register</h5>
             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
               <span aria-hidden="true">&times;</span>
             </button>
--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -1,27 +1,31 @@
 <template>
   <div class="login">
-    <quickModal>
-      <form v-if="loginForm" @submit.prevent="loginUser">
+    <LoginModal>
+      <form class="login-user" v-if="loginForm" @submit.prevent="loginUser">
         <input type="email" v-model="creds.email" placeholder="email">
         <input type="password" v-model="creds.password" placeholder="password">
-        <button type="submit">Login</button>
+        <button class="log-btn" type="submit">Login</button>
+        <div class="select" @click="loginForm = !loginForm">
+          <p v-if="loginForm">No account? <i class="fas fa-user-plus"></i></p>
+          <p v-else>Already have an account? <i class="fas fa-sign-in-alt"></i></p>
+        </div>
       </form>
-      <form v-else @submit.prevent="register">
+      <form class="login-user" v-else @submit.prevent="register">
         <input type="text" v-model="newUser.name" placeholder="name">
         <input type="email" v-model="newUser.email" placeholder="email">
         <input type="password" v-model="newUser.password" placeholder="password">
-        <button type="submit">Create Account</button>
+        <button class="log-btn" type="submit">Create</button>
+        <div class="select" @click="loginForm = !loginForm">
+          <p v-if="loginForm">No account? <i class="fas fa-user-plus"></i></p>
+          <p v-else>Already have an account? <i class="fas fa-sign-in-alt"></i></p>
+        </div>
       </form>
-    </quickModal>
-    <div @click="loginForm = !loginForm">
-      <p v-if="loginForm">No account? Click here to Register</p>
-      <p v-else>Already have an account? Click here to Login</p>
-    </div>
+    </LoginModal>
   </div>
 </template>
 
 <script>
-  import quickModal from "@/components/QuickModal"
+  import LoginModal from "@/components/LoginModal"
   export default {
     name: "login",
     data() {
@@ -47,7 +51,44 @@
       }
     },
     components: {
-      quickModal
+      LoginModal
     }
   };
 </script>
+
+<style scoped>
+  body {
+    height: 100vh;
+    width: 100%;
+    padding: 0;
+  }
+
+  #app {
+    height: 100%;
+    width: 100%;
+    display: flex;
+  }
+
+  .login {
+    display: flex;
+    width: 100%;
+    height: fit-content;
+    color: black;
+    background-color: #e4f9f5;
+  }
+
+  .login-user {
+    width: 20rem;
+    height: auto;
+    background-color: #5a5a5a8a;
+    color: #ffffff;
+    padding: 2rem;
+    text-align: left;
+    border-radius: 0.3125rem;
+    top: 50%;
+    left: 50%;
+    position: absolute;
+    transform: translate(-50%, -50%);
+    box-sizing: border-box;
+  }
+</style>
